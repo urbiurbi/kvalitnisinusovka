9cd21aa63ecadeff3c3726b496434e91bf449116
urbiurbi <mattyasurban@gmail.com>
2026-03-19 14:37:55 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class', // Důležité pro funkční přepínání témat
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      colors: {
-        amber: { 400: '#fbbf24', 500: '#f59e0b' },
-        blue: { 600: '#2563eb', 700: '#1d4ed8' }
-      }
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
     },
   },
   plugins: [],
